bd759d97932ede7c40fe91257bdfc6a699bab1af
Nuclear-Mango <jrivera012714@gmail.com>
2025-04-14 19:59:25 -0400

    Quick hacky solution for granting a 2x jump height mutation

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    [Tooltip("Dummy value for testing a hard coded mutation addition")]
+    private float testThreshold = 25f;
+    private PlayerMovement ratStat;
     public static GameManager Instance { get; private set; }
 
     private void Awake()
@@ -24,10 +28,21 @@ public class GameManager : MonoBehaviour
         UIManager.Instance.TriggerDeathEffect();
     }
 
+    //As PlayerMovement is currently the only player specific script, it is used as the reference type for now
+    public void RegisterPlayer(PlayerMovement player)
+    {
+        ratStat = player;
+    }
+
     public void OnThresholdTrigger(float threshold)
     {
         Debug.Log("Threshold " + threshold + "% reached.");
         //Handle mutation and potential environment updates from here
         //May send such logic to a different handler or entirely different manager later though depending on specificiations (MutationManager/EnvironmentManager?)
+        //Double the player's jump height for now
+        if (threshold == testThreshold && ratStat != null)
+        {
+            ratStat.JumpForce *= 2f;
+        }
     }
 }
