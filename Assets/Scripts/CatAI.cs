2687e5d1d5dbe84128b897b04792a255a535973d
dch82Drexel <44371537+DHuneke11@users.noreply.github.com>
2025-04-19 23:52:14 -0400

    Human/Cat Enemy AI Base Implementation
    The human and cat ais have been implemented on a base level. The living room scene with assets has been given navmeshes and example of how to set up the human and cat enemies.

---
+++ Assets/Scripts/CatAI.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CatAI : EnemyAi
+{
+    [SerializeField]
+    protected Transform[] tasks;
+
+    [SerializeField]
+    private int minTaskTime;
+
+    [SerializeField]
+    private int maxTaskTime;
+
+    protected NavMeshAgent agent;
+
+    private Transform player;
+
+    private bool playerFound;
+    private bool isInTask;
+    private bool isReacting;
+
+    private float taskTimer;
+    private float locationTime;
+
+    private Vector3 prevTask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = base.findPlayer();
+        agent = GetComponent<NavMeshAgent>();
+        isInTask = false;
+        isReacting = false;
+        prevTask = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerFound = base.isPlayerSighted(player) && base.isSightClear(player);
+        if (!isReacting)
+        {
+            if (playerFound)
+            {
+                Reaction();
+                isReacting = true;
+            }
+            else if (!isInTask)
+            {
+                prevTask = base.changeLocation(tasks, agent, prevTask);
+                locationTime = Random.Range(minTaskTime, maxTaskTime);
+                taskTimer = 0;
+                isInTask = true;
+            }
+            else
+            {
+                taskTimer += Time.deltaTime;
+                if (taskTimer >= locationTime)
+                {
+                    isInTask = false;
+                }
+            }
+        }
+        else
+        {
+            if (!playerFound)
+            {
+                isReacting = false;
+            }
+            else
+            {
+                Chase(player.position);
+            }
+        }
+    }
+
+    private void Reaction()
+    {
+        isInTask = false;
+        taskTimer = 0;
+    }
+
+    public void Chase(Vector3 playerPosition)
+    {
+        agent.SetDestination(playerPosition);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("player"))
+        {
+            print("Cat Kill");
+        }
+    }
+}
