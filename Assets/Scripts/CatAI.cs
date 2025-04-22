c9f3ac6d8af49f9c5d4916e5e2a1fcdfed9a996d
PrasinShrestha <65078211+PrasinShrestha@users.noreply.github.com>
2025-04-22 02:35:20 -0400

    All Stitched

--- Assets/Scripts/CatAI.cs
+++ Assets/Scripts/CatAI.cs
@@ -27,6 +27,8 @@ public class CatAI : EnemyAi
 
     private Vector3 prevTask;
 
+    [SerializeField] private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -68,10 +70,14 @@ public class CatAI : EnemyAi
         {
             if (!playerFound)
             {
+                agent.speed = 1;
+                anim.SetFloat("WalkSpeed", 0f);
                 isReacting = false;
             }
             else
             {
+                agent.speed = 3;
+                anim.SetFloat("WalkSpeed", 1.0f);
                 Chase(player.position);
             }
         }
