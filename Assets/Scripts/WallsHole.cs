0c4a03f929f7d9114b96fada3375401017b3d079
PrasinShrestha <65078211+PrasinShrestha@users.noreply.github.com>
2025-04-29 05:02:30 -0400

    Build ready, UI Shader settings

--- Assets/Scripts/WallsHole.cs
+++ Assets/Scripts/WallsHole.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public enum Axes { x, y, z }
