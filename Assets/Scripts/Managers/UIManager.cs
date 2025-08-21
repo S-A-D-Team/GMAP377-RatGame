2f2931778756f9bf071989a91a0b484b8f044fd8
LifeOfAtropos <158114850+LifeOfAtropos@users.noreply.github.com>
2025-08-21 09:54:56 -0400

    UIManager script
    Fixed Pause/Tutorial pause bug
    Removed Parameter script

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -3,52 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-public class Parameter
-{
-    private static int nextId = 0;
 
-    public int id;
-    public string label;
-    public object value;
-    public GameObject go;
-    public Parameter(string label, bool value, GameObject _go)
-    {
-        this.id = nextId++;
-        this.label = label;
-        this.value = value;
-        this.go = _go;
-    }
-    public Parameter(string label, string value, GameObject _go)
-    {
-        this.id = nextId++;
-        this.label = label;
-        this.value = value;
-        this.go = _go;
-    }
-
-    public int getID() 
-    {
-        return id;
-    }
-    public string getLabel()
-    {
-        return label;
-    }
-    public string GetDisplayValue()
-    {
-        if (value is bool boolVal)
-            return boolVal ? "true":"false";
-
-        return value?.ToString() ?? string.Empty;
-    }
-    public bool GetBoolValue()
-    {
-        if (value is bool boolVal)
-            return boolVal;
-
-        return false;
-    }
-}
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance { get; private set; }
@@ -96,13 +51,9 @@ public class UIManager : MonoBehaviour
     [SerializeField] private string deathCaption_Hunger;
     [SerializeField] private string deathCaption_Trap;
 
-
     //[SerializeField] private GameObject template_InputField;
     //[SerializeField] private GameObject template_Dropdown;
     //[SerializeField] private GameObject template_Toggle;
-    private List<Parameter> parameters;
-    private List<int> paramIDs;
-    //private bool paramUIChanged = false;
 
     [Space]  
     [SerializeField] private List<GameObject> pauseScreens;
@@ -113,8 +64,6 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Image ContaminationBar;
     [SerializeField] private GameObject InteractUI;
     [SerializeField] private GameObject InfectUI;
-
-
 
     [Space]
     public TextMeshProUGUI mutationPointsGainCueText;
@@ -134,7 +83,6 @@ public class UIManager : MonoBehaviour
         }
 
         Instance = this;
-        parameters = new List<Parameter>();
         pendingMutationCues = new Queue<string>();
         climbAnimator = climbIndicator.gameObject.GetComponent<Animator>();
     }
@@ -314,15 +262,14 @@ public class UIManager : MonoBehaviour
         {
             _uis.SetActive(false);
         }
-        Time.timeScale = 1.0f;
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-        /*if (paramUIChanged)
+
+        if (!isTutorialActive)
         {
-            updateParams?.Invoke(paramIDs);
-        }*/
-        //paramUIChanged = false;
-        //ClearParamUIListeners();
+            Time.timeScale = 1.0f;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
     }
 
     public void onPause()
@@ -414,215 +361,4 @@ public class UIManager : MonoBehaviour
         Cursor.visible = false;
 
     }
-
-    /*private void SetupParamUIListeners()
-    {
-        paramUIChanged = false;
-
-        foreach (var inputField in Parameters_UI_Object.GetComponentsInChildren<TMP_InputField>())
-        {
-            inputField.onValueChanged.AddListener((_) => paramUIChanged = true);
-        }
-
-        foreach (var toggle in Parameters_UI_Object.GetComponentsInChildren<Toggle>())
-        {
-            toggle.onValueChanged.AddListener((_) => paramUIChanged = true);
-        }
-
-    }*/
-
-    /*private void ClearParamUIListeners()
-    {
-        foreach (var inputField in Parameters_UI_Object.GetComponentsInChildren<TMP_InputField>())
-        {
-            inputField.onValueChanged.RemoveAllListeners();
-        }
-
-        foreach (var toggle in Parameters_UI_Object.GetComponentsInChildren<Toggle>())
-        {
-            toggle.onValueChanged.RemoveAllListeners();
-        }
-    }*/
-
-    /*
-    public int AddParameterOptionTextField(string label, string _initialValue = "Enter Here...")
-    {
-        GameObject _temp = Instantiate(template_InputField, Parameters_UI_Object.transform);
-        _temp.SetActive(true);
-
-        Parameter newParam = new Parameter(label, _initialValue, _temp);
-
-        _temp.GetComponentInChildren<TextMeshProUGUI>().text = label;
-        TMP_InputField inputField = _temp.GetComponentInChildren<TMP_InputField>();
-        inputField.text = _initialValue;
-        // Sync UI to Parameter
-        inputField.onValueChanged.AddListener((string val) =>
-        {
-            newParam.value = val;
-        });
-
-        parameters.Add(newParam);
-
-
-        return newParam.getID();
-    }
-    public int AddParameterOptionToggle(string label, bool _initialToggle = false)
-    {
-        GameObject _temp = Instantiate(template_Toggle, Parameters_UI_Object.transform);
-        _temp.SetActive(true);
-
-        Parameter newParam = new Parameter(label, _initialToggle, _temp);
-
-        _temp.GetComponentInChildren<TextMeshProUGUI>().text = label;
-        Toggle Toggle = _temp.GetComponentInChildren<Toggle>();
-        Toggle.onValueChanged.AddListener((bool val) =>
-        {
-            newParam.value = val;
-        });
-
-        parameters.Add(newParam);
-        return newParam.getID();
-    }
-    public int AddParameterOptionDropMenu(string label, List<string> options, int initialIndex)
-    {
-        GameObject _temp = Instantiate(template_Dropdown, Parameters_UI_Object.transform);
-        _temp.SetActive(true);
-
-        //Thanks ChatG
-        _temp.GetComponentInChildren<TextMeshProUGUI>().text = label;
-        TMP_Dropdown dropdown = _temp.GetComponentInChildren<TMP_Dropdown>();
-        dropdown.ClearOptions();
-        dropdown.AddOptions(options);
-        dropdown.value = initialIndex;
-        dropdown.RefreshShownValue();
-
-        Parameter newParam = new Parameter(label, options[initialIndex], _temp);
-        // Sync UI to Parameter
-        dropdown.onValueChanged.AddListener((int index) =>
-        {
-            newParam.value = options[index];
-        });
-        parameters.Add(newParam);
-
-        return newParam.getID();
-    }*/
-
-    /*
-    public string getParamName(int id)
-    {
-        foreach (Parameter param in parameters)
-        {
-            if (param.getID() == id) return param.label;
-        }
-        return "null";
-    }
-
-    public object getParamValue(int id)
-    {
-        foreach (Parameter param in parameters)
-        {
-            if(param.getID() == id) return param.value;
-        }
-        return -1;
-    }
-
-    public bool setParamValue(int id, object value)
-    {
-        foreach (Parameter param in parameters)
-        {
-            if (param.getID() == id)
-            {
-                param.value = value;
-
-                TMP_InputField inputField = param.go.GetComponentInChildren<TMP_InputField>();
-                if (inputField != null)
-                {
-                    inputField.text = value?.ToString() ?? "";
-                    return true;
-                }
-
-                TMP_Dropdown dropdown = param.go.GetComponentInChildren<TMP_Dropdown>();
-                if (dropdown != null)
-                {
-                    if (value is string valStr)
-                    {
-                        int index = dropdown.options.FindIndex(opt => opt.text == valStr);
-                        if (index >= 0)
-                        {
-                            dropdown.value = index;
-                            dropdown.RefreshShownValue();
-                            return true;
-                        }
-                    }
-                    else if (value is int indexVal && indexVal >= 0 && indexVal < dropdown.options.Count)
-                    {
-                        dropdown.value = indexVal;
-                        dropdown.RefreshShownValue();
-                        return true;
-                    }
-                }
-
-                Toggle Toggle = param.go.GetComponentInChildren<Toggle>();
-                if (Toggle != null && value is bool boolVal)
-                {
-                    Toggle.isOn = boolVal;
-                    return true;
-                }
-
-                return false;
-            }
-        }
-        return false;
-    }
-    [Space]
-    [Header("Parameters")]
-
-    [SerializeField] private GameObject Parameters_UI_Object;
-    public Toggle biteToggle;
-
-    public void onBiteToggleChange()
-    {
-        GameObject.FindWithTag("player").GetComponent<Hole>().isEnabled = biteToggle.isOn;
-    }
-
-    
-    public Toggle ClimbToggle;
-    public void onClimbToggleChange()
-    {
-        GameManager.Instance.onClimbToggle(ClimbToggle.isOn);
-    }
-
-    public TMP_InputField jumpStack;
-    public void onJumpStackChange()
-    {
-        try
-        {
-            int newVal = int.Parse(jumpStack.text);
-            GameManager.Instance.onJumpStackChange(newVal);
-        }
-        catch (System.Exception)
-        {
-
-            throw;
-        }
-
-    }
-
-    public TMP_InputField speedStack;
-    public void onspeedStackChange()
-    {
-        try
-        {
-            int newVal = int.Parse(speedStack.text);
-            GameManager.Instance.onSpeedStackChange(newVal);
-        }
-        catch (System.Exception)
-        {
-
-            throw;
-        }
-
-    }
-    */
 }
-
